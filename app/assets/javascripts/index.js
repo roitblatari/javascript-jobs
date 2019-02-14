9e8a130ad6e9beac89a724ce059e6d38adc88f3c
roitblatari <roitblatari@gmail.com>
2019-02-13 20:42:07 -0500

    ajax home

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,209 @@
 $(function () {
   // clearAppDiv() 
   console.log("app/assets/javascripts/index.js")
-  employeeLoginForm()
-  employerLoginForm()
+  employeeLoginForm();
+  // $('a#employee-login').on("click", (e) => employerLoginForm(e))
+  employerLoginForm();
+  // $('a#employee-login').on("click", (e) => employerLoginForm())
   // employerNewForm() 
-  employeeNewForm()
-  jobIndex()
+  employeeNewForm();
+  $('a#created-jobs').on('click', (e) => jobIndex());
   
   // $('button#past-jobs-employee-jobs-path').on("click", pastJobIndex(e)); 
   $('a.navbar-brand#root').on('click', (e) => rootPage(e));
-  $('a#home').on("click", (e) =>  home(e))
+  $('a#home').on("click", (e) => home(e))
+  $('#past-jobs-employee').on("click", (e) => pastJobsIndex(e))
   // $('span#new_employee').on("click", employeeNewForm);
   // $('#new_employee_form').on("submit", submitEmployeeNewForm )
 
 })
 
+const employeeLoginForm = () => {
+  $('#employee-login').on("click", function (e) {
 
+    e.preventDefault()
+    // debugger
+    clearWelcome()
+
+    $.ajax({
+      url: 'http://0.0.0.0:3000/signin_employee',
+      method: 'GET',
+      dataType: 'html'
+    }).success(function (response) {
+      $('div#app-div-id').html(response);
+      submitEmployeeLoginForm()
+    })
+  })
+}
+// const employeeLoginForm = () => {
+//   $('a#employee-login').on("click", function (e) {
+
+//     e.preventDefault()
+//     // debugger
+//     clearWelcome()
+
+//     $.ajax({
+//       url: 'http://0.0.0.0:3000/signin_employee',
+//       method: 'GET',
+//       dataType: 'html'
+//     }).success(function (response) {
+//       $('div#app-div-id').html(response);
+//       submitEmployeeLoginForm()
+//     })
+//   })
+// }
+
+const home = (e) => {
+  // $('a#home').on("click", function (e) {
+    debugger
+  e.preventDefault()
+  // debugger
+  $.ajax({
+    url: e.currentTarget["href"],
+    method: 'GET',
+    dataType: 'html'
+  }).success(function (response) {
+    // debugger
+    $('body').html(response)
+    // submitEmployeeNewForm();
+    upcomingJobs();
+  })
+  // })
+}
+
+var employeeNewForm = () => {
+  $('a#new_employee').on("click", function (e) {
+    e.preventDefault()
+
+    clearWelcome()
+    $.ajax({
+      url: 'http://0.0.0.0:3000/employees/new',
+      method: 'GET',
+      dataType: 'html'
+    }).success(function (response) {
+      $('div#app-div-id').html(response)
+      submitEmployeeNewForm();
+    })
+  })
+}
+
+const submitEmployeeNewForm = (e) => {
+  $('form#new_employee_form').on("submit", function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: "POST",
+      data: $(this).serialize(),
+      dataType: 'json',
+      url: this.action
+    }).success(function (response) {
+
+      let employee = new Employee(response)
+      let employeeHtml = employee.employeeHTML()
+      deleteNewEmployeeForm()
+      $('div#employee-div-id').html(employeeHtml)
+
+    })
+  })
+}
+
+const submitEmployeeLoginForm = () => {
+  $('form#new_session_form').on("submit", function (e) {
+    //  debugger
+    e.preventDefault()
+
+    $.ajax({
+      type: "POST",
+      data: $(this).serialize(),
+      dataType: 'json',
+      url: this.action //'session/create_employee'
+    }).success(function (response) {
+      // debugger
+      let employee = new Employee(response)
+      let employeeHtml = employee.employeeHTML()
+      deleteLoginEmployeeForm()
+      $('div#employee-div-id').html(employeeHtml)
+      pastJobsIndex()
+      upcomingJobs()
+    }).error(function (a, err) {
+      debugger
+      // console.log("err:" , err)
+    })
+
+  })
+}
+
+
+
+function clearWelcome() {
+  $('div#the-welcome-page').html("")
+}
+function clearAppDivId() {
+  $('div#app-div-id').html("")
+}
+function deleteNewEmployeeForm() {
+  $('#new_employee_form').html("")
+}
+function deleteLoginEmployeeForm() {
+  $('#new_session_form').html("")
+}
+
+var rootPage = (e) => {
+  e.preventDefault()
+  $.ajax({
+    url: 'http://0.0.0.0:3000/',
+    method: 'GET',
+    dataType: 'html'
+  }).success(function (response) {
+    $('div#employee-div-id').html(response)
+  })
+}
+
+// define Employee class
+class Employee {
+  constructor(obj) {
+    this.name = obj.name;
+    this.email = obj.email;
+    this.profession = obj.profession;
+    this.id = obj.id;
+  }
+}
+{/* <a href="/employees/${this.id}/jobs/past_jobs" class="btn btn-light" id="past-jobs-employee-jobs-path" >Past Jobs</a> */ }
+Employee.prototype.employeeHTML = function () {
+  return (`
+    <div class='employee-show'>
+    <p> ${this.id}</p>
+		<h3>${this.name}</h3>
+    <p> ${this.email}</p>
+    <p> ${this.profession}</p>
+   
+
+    <button type="button" class="btn btn-light">
+    <a id="past-jobs-employee" href="/employees/${this.id}/jobs/past_jobs">Past Jobs</a> 
+   
+    </button>
+    <button type="button" class="btn btn-light">
+      <a id="upcoming-jobs" href="/employees/${this.id}/jobs/upcoming_jobs"  >Upcoming Jobs</a>
+    </button>
+
+
+    
+    </div>
+    
+  `)
+}
+
+// //////////////// Employee /////////////////////
+// 
+// //////////////// Employee /////////////////////
+// 
+// //////////////// Employee /////////////////////
+// 
+// //////////////// Employee /////////////////////
+// 
+// //////////////// Employee /////////////////////
+// 
+// //////////////// Employee /////////////////////
+// 
 // //////////////// Employee /////////////////////
 // 
 
